0023cfdb58ae5a7fb67b2dcc0f8628291df3bfc5
azaz111 <86608104+azaz111@users.noreply.github.com>
2021-12-14 10:26:53 +0300

    Add files via upload

--- auto_bash.bash
+++ auto_bash.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+ #!/bin/bash
 # ----
 cd /root
 sudo apt-get update 
@@ -33,26 +33,28 @@ screen -dmS mount rclone mount --daemon baws32: /baws32
 # Четвкртый --------------------------------------------------
 cd /root
 # Монтируем диск 1
-parted /dev/nvme0n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
-mkfs.xfs -f /dev/nvme0n1
-partprobe /dev/nvme0n1
+parted /dev/nvme2n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
+mkfs.xfs -f /dev/nvme2n1
+partprobe /dev/nvme2n1
 mkdir /disk1
-mount /dev/nvme0n1 /disk1
+mount /dev/nvme2n1 /disk1
 # Монтируем диск 2
 parted /dev/nvme1n1 --script mklabel gpt mkpart xfspart xfs 0% 100%
 mkfs.xfs -f /dev/nvme1n1
 partprobe /dev/nvme1n1
-mkdir /disk2
-mount /dev/nvme1n1 /disk2
+mkdir /disk4
+mount /dev/nvme1n1 /disk4
 # Создаем дериктории на дисках
 cd /disk1
 mkdir vid1 
-cd /disk2
+cd /disk4
 mkdir vid2
-cd /disk1
+cd /disk4
 mkdir video1
-cd /disk2
+cd /disk4
 mkdir video
+cd /disk4
+mkdir vid1
 cd /root
 # Качаем плоттер и устанавливаем 
 cd /root
@@ -65,12 +67,12 @@ sleep 3
 sleep 5
 cd /root
 # отправка на сервер информационного файла ------------------------------
-server=$(sed -n '1,1p' < aknomber.txt)
-kakoi=$(sed -n '2,2p' < aknomber.txt) # .txt
-kuda=$(sed -n '3,3p' < aknomber.txt)
+server=$(sed -n '1,1p' < cd /root/aknomber.txt)
+kakoi=$(sed -n '2,2p' < cd /root/aknomber.txt) # .txt
+kuda=$(sed -n '3,3p' < cd /root/aknomber.txt)
 
 # - deriktoriya usera
-echo -e "#!/usr/bin/expect\nsleep 270\nspawn ssh -oStrictHostKeyChecking=no root@45.79.151.232" > /root/dir_user.sh
+echo -e "#!/usr/bin/expect\nsleep 260\nspawn ssh -oStrictHostKeyChecking=no root@45.79.151.232" > /root/dir_user.sh
 echo 'expect "password"' >> /root/dir_user.sh
 echo 'send "XUVLWMX5TEGDCHDU\r"' >> /root/dir_user.sh
 echo 'expect -re "# $"' >> /root/dir_user.sh
@@ -80,7 +82,7 @@ chmod 777 dir_user.sh
 screen -dmS puti1 ./dir_user.sh 
 
 # - deriktoriya akka
-echo -e "#!/usr/bin/expect\nsleep 270\nspawn ssh -oStrictHostKeyChecking=no root@45.79.151.232" > /root/dir_akka.sh
+echo -e "#!/usr/bin/expect\nsleep 280\nspawn ssh -oStrictHostKeyChecking=no root@45.79.151.232" > /root/dir_akka.sh
 echo 'expect "password"' >> /root/dir_akka.sh
 echo 'send "XUVLWMX5TEGDCHDU\r"' >> /root/dir_akka.sh
 echo 'expect -re "# $"' >> /root/dir_akka.sh
@@ -100,8 +102,8 @@ screen -dmS otpravka_na_serv ./otpravka.sh
 # ЗАпуск Плотера ------------------------------
 screen -dmS Copi1 ./Copi1.sh
 screen -dmS Copi ./Copi.sh
-screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 12 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
-screen -dmS videorender2 ./chia-plotter/build/chia_plot -n -1 -r 12 -u 256 -t /disk1/vid1/ -2 /disk2/vid2/ -d /disk3/video1/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
+screen -dmS videorender1 ./chia-plotter/build/chia_plot -n -1 -r 12 -u 256 -t /disk1/vid1/ -2 /disk4/vid1/ -d /disk4/video/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
+screen -dmS videorender2 ./chia-plotter/build/chia_plot -n -1 -r 12 -u 256 -t /disk4/vid1/ -2 /disk4/vid2/ -d /disk4/video1/ -f b8e1d57e3e2dbb40ac8f2b257b762d05fcfc5b79c32a22255424644b7d183daa7c454624783f2d959c02eb1d2a4ba3a3 -p 91ea997633345082b15f83b957449180037030b6b7485f07ed4ee7558d08d3efbccf2c3d68ba724f5b3a8281a0055e27
 screen -dmS trans ./trans.sh
 screen -dmS otchet python3 awsstat.py
 screen -r trans
